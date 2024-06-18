a251bed0b294dfe995775fb8d7a02f92ffa0ffe1
srinathbondala <srinath.b2004@gmail.com>
2024-06-18 14:00:48 +0530

    update payment

---
+++ scripts/payment.js
@@ -0,0 +1,5 @@
+function ShowAdress(){
+    document.getElementById("shipheading").textContent = "Choose a shipping address";
+    document.getElementsByClassName("Addressoption")[0].style.display = "block";
+    document.getElementById("AllAdress").style.display = "block";
+}
